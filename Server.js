5e394d5006fbb65358f643cc046b910405f16a99
deep <d33psutariya@gmail.com>
2023-08-29 08:53:40 +0530

    Last One

--- Server.js
+++ Server.js
@@ -18,7 +18,7 @@ app.use(require('./routes/AuthenticationRoutes'));
 app.use(require('./routes/CardRoutes'));
 
 require('dotenv').config()
-const PORT = process.env.PORT || 8000;
+const PORT = 5000;
 
 app.listen(PORT,()=>{
     console.log("Listening on " + PORT);
